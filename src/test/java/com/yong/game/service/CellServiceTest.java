aadd8b95bb140cf18160a6954e098ae2ec7901cb
yongliu <yong.liu2001@gmail.com>
2018-07-02 02:52:48 +1000

    Create Unit test class for CellService and GameBoardService; add junit, mockito, assertj to pom

---
+++ src/test/java/com/yong/game/service/CellServiceTest.java
@@ -0,0 +1,27 @@
+package com.yong.game.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by yongliu on 2/7/18.
+ */
+public class CellServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getNeighbourCells() throws Exception {
+
+
+
+    }
+
+    @Test
+    public void calculateStatusBaseOnNeighbours() throws Exception {
+    }
+
+}
---
+++ src/test/java/com/yong/game/service/GameBoardServiceTest.java
@@ -0,0 +1,25 @@
+package com.yong.game.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for GameBordService
+ * Created by yongliu on 2/7/18.
+ */
+public class GameBoardServiceTest {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void initialGameBoardWithCoordinates() throws Exception {
+    }
+
+    @Test
+    public void executeStages() throws Exception {
+    }
+
+}
